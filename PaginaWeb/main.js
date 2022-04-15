5d381696213d2f07836aa5acb911cb0964df9bea
brashian <bsandovala12@gmail.com>
2022-04-15 20:45:17 -0300

    Finiquito mi participaion

--- PaginaWeb/main.js
+++ PaginaWeb/main.js
@@ -1,12 +1,22 @@
 
+/* const show_name = () => {
+    let name = localStorage.getItem("nombre")
+    document.getElementById("h_nombre").textContent = `Welcome User - ${name}`
+}
+
+document.getElementById("body_eng").onload = show_name();
+
+*/
 
 const mostra_nombre = () => {
     //succiono datos del almacen local, con su
     let nombre = localStorage.getItem('nombre')
-    document.getElementById("h_nombre").textContent = `Welcome User - ${nombre}`
+    
+        document.getElementById("h_nombre").textContent = nombre
 }
 //Evento onload, al cargar ejecuta la funcion mostra_nombre
 document.getElementById("body_focus").onload = mostra_nombre()
+
 
 const cambio_idiomas = () => {
     let language = document.getElementById('option').value
